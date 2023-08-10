17b1bf045f74ac9fbace2136c8715df321cc01a3
Zhanna Kulaginae <kuljeanne@gmail.com>
2023-08-10 18:51:33 +0300

    feat: finish server

--- server/controller/user.contriller.ts
+++ server/controller/user.contriller.ts
@@ -9,13 +9,26 @@ class UserController {
     )
     res.json(newUser.rows[0])
   }
+
   async getUsers(req, res) {
     const users = await db.query('SELECT * FROM users')
-
     res.json(users.rows)
   }
-  async deleteUser(req, res) {}
-  async updateUser(req, res) {}
+
+  async updateUser(req, res) {
+    const { id, name, surname } = req.body
+    const user = await db.query(
+      'UPDATE users set name = $1, surname = $2 where id = $3 RETURNING *',
+      [name, surname, id]
+    )
+    res.json(user.rows[0])
+  }
+  
+  async deleteUser(req, res) {
+    const { id } = req.body
+    const user = await db.query('DELETE FROM users where id = $3', [id])
+    res.json(user.rows[0])
+  }
 }
 
 module.exports = new UserController()
